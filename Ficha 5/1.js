ff284fd63ea853e4f1163f89ab56573562baa8f2
amorima <work@antonioamorim.pt>
2024-12-19 00:26:54 +0000

    Aulas

---
+++ Ficha 5/1.js
@@ -0,0 +1,11 @@
+let numero = -1
+
+if (numero > 0) {
+    console.log("Número Positivo")
+} 
+else if (numero == 0) {
+    console.log("Zero")
+}
+else if (numero < 0) {
+    console.log("Número Negativo")
+}
---
+++ Ficha 5/2.js
@@ -0,0 +1,9 @@
+let numero1 = 1
+let numero2 = 1
+
+if (numero1 == numero2) {
+    console.log("Números Iguais")
+}
+else {
+    console.log("Números Diferentes")
+}
